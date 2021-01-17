7c79d6a893012b2004df6cea7f9139df4bd5db09
Britani Walley <britaniwalley@outlook.com>
2021-01-16 21:17:07 -0700

    new JS sheet, added if/else statements

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
-// button do something
-// generating a set length password from 1 array and console.log
-//connect the 2
+//Assignment code (variables)
 var generateBtn = document.querySelector("#generate");
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-var lowercase = "abcdefghijklmnopqrstuvwxyz".split("")
-var numbers = ("123456789").split("")
-var specCharacters = ("!@#$%^&").split("")
-var passwordLength = "8"
-var selectedCharacters = []
-var password = ""
+var passwordText = document.querySelector("#password");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers = ("123456789").split("");
+var specCharacters = ("!@#$%^&").split("");
+var passwordLength = "8";
+var selectedCharacters = [];
+var password = [];
+var length = "";
 
 function writePassword() {
-  console.log(password);
-}
-function userOptions() {
-  var length = (
-    prompt("Please select a length between 8 and 128 characters")
-  );
-  if (isNaN(length) === true) {
-    alert("Please enter a number between 8 and 128");
+   length = prompt("Please select a length between 8 and 128 characters")
+   if(length < 8 || length > 128 || isNaN(length)) {
+    alert("Your password must be between 8 and 128 characters");
     return;
-  }
-
-  if (length < 8) {
-    alert("Your password must be more than 8 characters");
+   }
+   if(uppercase) {
+    confirm("Click okay to include lowercase letters, click cancel to not include");
+    console.log(uppercase)
     return;
-  } 
-  if (length > 128) {
-    alert("Your password must be less than 129 characters");
+   } else {
+     console.log("don't include uppercase letters")
+
+   }
+   if(lowercase) {
+    confirm("Click okay to include lowercase letters, click cancel to not include");
     return;
-  }
-  //WHEN prompted for character types to include in the password 
-  //THEN I choose useLowercase, uppercase, numeric, and/or special characters
-
-  var uselowercase = confirm("Click okay to include lowercase letters, click cancel to not include");
-    var useuppercase = confirm("Click okay to include uppercase letters, click cancel to not include");
-    var usenumbers = confirm("Click okay to include numbers, click cancel to not include"); 
-    var usespecCharacters = confirm("Click okay to include special characters, click cancel to not include");
-
-  if (
+   }
+   if(numbers) {
+    confirm("Click okay to include numbers, click cancel to not include");
+    return;
+   }
+   if(specCharacters) {
+    confirm("Click okay to include special characters, click cancel to not include");
+    return;
+   }
+   if (
     uselowercase === false &&
-    useuppercase === false && 
-    usenumbers === false && 
-    usespecCharacters === false 
+    useuppercase === false &&
+    usenumbers === false &&
+    usespecCharacters === false
   ) {
     alert("You must choose at least 1 criteria for your password");
     return;
 
   }
+
+passwordText = document.querySelector("#password");
+console.log(password)
+passwordText.textContent = password
+
+
+
 }
+
+
   
-  // user options
-  
-  function generatePassword() {
+ 
 
-    //for loop 
-    for (i = 0; i < passwordLength; i++) {
-      // produce a random lowercase letter 8 times 
-      password = password + selectedCharacters[Math.floor(math.random() * selectedCharacters.length)]
-      // add random letter to our password string var
-    }
-    writePassword()
+//   //for loop 
+  // for (i = 0; i < passwordLength; i++) {
+  //   // produce a random lowercase letter 8 times 
+  //   var password =  password + selectedCharacters[Math.floor(math.random() * selectedCharacters.length)]
+  //   // add random letter to our password string var 
+  //   password = password + selectedCharacters[Math.floor(math.random() * selectedCharacters.length)]
+  // }
 
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", userOptions);
-
-  passwordLength = length
-
-  generatePassword()
+// }
+// Add event listener to generate button
--- script2.js
+++ script2.js
@@ -1,12 +1,75 @@
-// button do something
-// generating a set length password from 1 array and console.log
-//connect the 2
+//Assignment code (variables)
 var generateBtn = document.querySelector("#generate");
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-var lowercase = "abcdefghijklmnopqrstuvwxyz".split("")
-var numbers = ("123456789").split("")
-var specCharacters = lowerCase.concat(numbers)
-var passwordLength = "8"
-var selectedCharacters = []
-var password = ""
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers = ("123456789").split("");
+var specCharacters = ("!@#$%^&").split("");
+var passwordLength = "8";
+var selectedCharacters = [];
+var password = [];
+var length;
+var passwordText = document.querySelector("#password");
+
+function writePassword() {
+   
+    // password.splice(0, password.length);
+
+   length = prompt("Please select a length between 8 and 128 characters")
+   console.log(length);
+   if(length < 8 || length > 128 || isNaN(length)) {
+    alert("Your password must be between 8 and 128 characters");
+    return;
+   }
+   if(uppercase) {
+    confirm("Click okay to include uppercase letters, click cancel to not include");
+    selectedCharacters = selectedCharacters.concat(uppercase)
+    console.log("include uppercase letters");
+   } else {
+       console.log("don't include uppercase letters");
+       return;
+   }
+   if(lowercase) {
+    confirm("Click okay to include lowercase letters, click cancel to not include");
+    selectedCharacters = selectedCharacters.concat(lowercase)
+    console.log("include lowercase letters");
+   } else {
+       console.log("don't include lowercase letters");
+       return;
+   }
+   if(numbers) {
+    confirm("Click okay to include numbers, click cancel to not include"); 
+    selectedCharacters = selectedCharacters.concat(numbers)
+    console.log("include numbers");
+   } else {
+       console.log("don't include numbers");
+       return;
+   }
+   if(specCharacters) {
+    confirm("Click okay to include special characters, click cancel to not include"); 
+    selectedCharacters = selectedCharacters.concat(specCharacters)
+    console.log("include special characters");
+   } else {
+       console.log("don't include special characters");
+       return;
+   } 
+generatePassword();
+console.log(selectedCharacters)
+} 
+
+generateBtn.addEventListener("click", writePassword); 
+
+function generatePassword() {
+  for (var i=0; i<passwordLength; i++) {
+    var random = [Math.floor(Math.random() * selectedCharacters.length)]
+    password.push(selectedCharacters[random]);
+    passwordText.textContent = password.join("");
+  }
+
+} 
+
+
+
+
+
+
 
